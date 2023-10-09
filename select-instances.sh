2573f5ce1ed4c7d72f543d46937ffe5eddfc5305
Raúl Martín <raul.martin@urjc.es>
2023-10-09 19:36:04 +0200

    Instances selected, ready to roll

--- select-instances.sh
+++ select-instances.sh
@@ -1,6 +1,6 @@
 set -e
 mvn clean package
-java -jar target/VRPOD-0.18-SNAPSHOT.jar --instance-selector \
+java -jar target/VRPOD-0.19-SNAPSHOT.jar --instance-selector \
 --instances.preliminar-output-path=instances/tuning \
 --instances.for-selection=instances/all \
---instances.preliminar-percentage=0.05
+--instances.preliminar-percentage=0.15
